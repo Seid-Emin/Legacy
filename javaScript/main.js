e17bd2cb817e2465ea45352a568a48696f3ae3d4
Seid-Emin <seid.saydahmed@abv.bg>
2020-01-17 23:33:17 +0200

    Hover effect on works added Links fixed

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
 
+    // toggle menu with animations
+    $('.menu-icon').click(function () {
+        $('.menu-icon-bar1').toggleClass('bar1-active');
+        $('.menu-icon-bar2').toggleClass('bar2-active');
+        $('.menu-icon-bar3').toggleClass('bar3-active');
+        $('.nav').toggleClass('navActive');
+    });
+
+
+    // hover efect on projects
+    $('.works_project-link-list li').hover(function () {
+        var classOfHoveredLink = $(this).attr('class').replace('works_project-link-list-item ', '');
+        var imgClassToShow = $('.works_bg-project-image-wrap-inner div').hasClass(`works_bg-project-${classOfHoveredLink}`);
+        if (imgClassToShow) {
+            $('.works_bg-project-' + classOfHoveredLink).addClass('projectBG-active');
+        }
+    }, function () {
+        $('.projectBG-active').removeClass('projectBG-active');
+    });
+
+
+
+
 });
